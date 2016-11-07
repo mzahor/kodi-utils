b305c83218a927bf1bfe32e1c063a29d15e3100d
Marian Zagoruiko <marian.zagoruiko@gmail.com>
2016-11-07 21:09:26 +0200

    change kodi IP

--- chrome-extension/play2kodi.js
+++ chrome-extension/play2kodi.js
@@ -2,7 +2,7 @@ chrome.browserAction.onClicked.addListener((tab) => {
     var headers = new Headers();
     headers.set('Content-Type', 'application/json');
 
-    fetch('http://localhost:5000/player', {
+    fetch('http://192.168.0.105:5000/player', {
         method: 'POST',
         headers: headers,
         body: JSON.stringify({
